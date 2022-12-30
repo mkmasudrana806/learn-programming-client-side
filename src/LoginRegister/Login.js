4e6236bd15421a016149c897ab48645aa82bf718
mkmasudrana806 <mkmasudrana806@gmail.com>
2022-12-30 21:40:49 +0600

    login page done

--- src/LoginRegister/Login.js
+++ src/LoginRegister/Login.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Login.css";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const Login = () => {
-    return (
-        <div>
-            <h3>this is login page</h3>
+  return (
+     <div className="form-width">
+      <h5 className="login">Log In</h5>
+      <Form className="mx-auto">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <h6 className="text-primary">Forgot Password?</h6>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Accept Terms & Condition" />
+        </Form.Group>
+        <div className="d-grid gap-2">
+          <Button variant="primary" size="md">
+            Sign In
+          </Button>
         </div>
-    );
+      </Form>
+      <p className="mt-2">
+        Don't Have An Account? <Link to="/register">Create New Account</Link>
+      </p>
+      <p className="or">Or</p>
+      <div className="d-grid gap-2">
+        <Button variant="outline-success" size="md">
+          <FaGoogle></FaGoogle> Sign in with Google
+        </Button>
+        <Button variant="outline-success" size="md">
+          <FaGithub></FaGithub> Sign in with GitHub
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/LoginRegister/Register.js
+++ src/LoginRegister/Register.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { FaGithub, FaGoogle } from "react-icons/fa";
+
 
 const Register = () => {
     return (
-        <div>
-           <h3>h3llo
-            
-           </h3>
-        </div>
+        <div className="form-width">
+        <h5 className="login">Sign Up</h5>
+        <Form className="mx-auto">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Accept Terms & Condition" />
+          </Form.Group>
+          <div className="d-grid gap-2">
+            <Button variant="primary" size="md">
+              Sign Up
+            </Button>
+          </div>
+        </Form>
+        <p className="mt-2">
+          Already Have An Account? <Link to="/login">Log In</Link>
+        </p>
+      </div>
     );
 };
 
