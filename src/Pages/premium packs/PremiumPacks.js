b93e98089602bf3f2bbe1fd343ec04c35b6ce3a6
mkmasudrana806 <mkmasudrana806@gmail.com>
2023-09-06 17:47:00 +0600

    responsive done

--- src/Pages/premium packs/PremiumPacks.js
+++ src/Pages/premium packs/PremiumPacks.js
@@ -10,29 +10,34 @@ const PremiumPacks = () => {
   // premium packs data load from backend
   const [packs, setPacks] = useState([]);
   useState(() => {
-    fetch("http://localhost:5000/premium-packs")
+    fetch(
+      "https://b610-lerning-platform-server-side-mkmasudrana806.vercel.app/premium-packs"
+    )
       .then((res) => res.json())
       .then((data) => setPacks(data));
   }, []);
 
   return (
     <div className="py-5">
-      <div className="d-flex  justify-content-between">
+      <div>
         <div className=" d-flex gap-1">
           <TbPackages size={34}></TbPackages>{" "}
-          <div>
-            <h2 className="ms-2 fw-bold">
-              Premium <span className="text-primary"> Packs</span>
-            </h2>
+          <div className="w-100">
+            <div className="view-more d-flex justify-content-between">
+              <h2 className="ms-2 fw-bold">
+                Premium <span className="text-primary"> Packs</span>
+              </h2>
+              <Link
+                to="/all-premium-packs"
+                className="btn btn-primary py-2 px-3"
+              >
+                View More <FaArrowRightLong></FaArrowRightLong>
+              </Link>
+            </div>
             <p className="m-0">
               Curated courses packed otgether from expert tutors
             </p>
           </div>
-        </div>
-        <div className="view-more">
-          <Link to="/all-premium-packs" className="btn btn-primary">
-            View More <FaArrowRightLong></FaArrowRightLong>
-          </Link>
         </div>
       </div>
       {/* premium packs container */}
